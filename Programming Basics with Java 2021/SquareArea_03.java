738f159bed60aa4b329c3ce803661e6196534b61
AngelJava123 <88735287+AngelJava123@users.noreply.github.com>
2021-08-13 11:38:22 +0300

    Add files via upload
    Sorting out square area

---
+++ Programming Basics with Java 2021/SquareArea_03.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class SquareArea_03 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner (System.in);
+        int a = Integer. parseInt(scan.nextLine());
+        int area = a * a;
+        System.out.println(area);
+
+
+    }
+}
