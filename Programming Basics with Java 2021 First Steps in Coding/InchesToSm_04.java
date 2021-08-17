30d06515437b37843dc418e533423d56acab9e6a
AngelJava123 <88735287+AngelJava123@users.noreply.github.com>
2021-08-18 01:29:04 +0300

    Add files via upload
    Initial Coding with Java language

---
+++ Programming Basics with Java 2021 First Steps in Coding/InchesToSm_04.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class InchesToSm_04 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double inches = Double.parseDouble(scanner.nextLine());
+        double centimeters = inches * 2.54;
+        System.out.println(centimeters);
+    }
+}
