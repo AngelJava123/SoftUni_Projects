ba0c9ead1fda7455bad6d708706c77a0dc658767
AngelJava123 <88735287+AngelJava123@users.noreply.github.com>
2021-08-21 00:30:17 +0300

    Add files via upload
    printing sentence depending on whether it is sunny or not outside

---
+++ First Steps in Coding More Exercise/WeatherForecast_09.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class WeatherForecast_09 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String whether = scanner.nextLine();
+
+        if (whether.equals("sunny")) {
+            System.out.println("It's warm outside!");
+        } else {
+            System.out.println("It's cold outside!");
+        }
+    }
+}
