fc04dfe3bbdc324a41d95ca781595c32d851d3d1
AngelJava123 <88735287+AngelJava123@users.noreply.github.com>
2021-08-24 12:58:50 +0300

    Add files via upload

---
+++ For Loop Exercise/HalfSumElement_02.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class HalfSumElement_02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            int number = Integer.parseInt(scanner.nextLine());
+            sum = sum + number;
+
+            if (number > max) {
+                max = number;
+            }
+        }
+        sum = sum - max;
+
+        if (max == sum) {
+            System.out.println("Yes");
+            System.out.printf("Sum = %d", max);
+        } else {
+            System.out.println("No");
+            System.out.printf("Diff = %d", Math.abs(sum - max));
+        }
+    }
+}
