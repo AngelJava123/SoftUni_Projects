8dc3d9b782435b3ebe3e27234df40de2e660215e
AngelJava123 <88735287+AngelJava123@users.noreply.github.com>
2021-08-29 23:24:51 +0300

    Add files via upload

---
+++ Nested Loops Lab/Travelling_05.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Travelling_05 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String destination = scanner.nextLine();
+
+        while (!destination.equals("End")) {
+            double excursionPrice = Double.parseDouble(scanner.nextLine());
+
+            double availableMoney = 0;
+
+            while (availableMoney < excursionPrice) {
+                double savedMoney = Double.parseDouble(scanner.nextLine());
+                availableMoney += savedMoney;
+            }
+            System.out.printf("Going to %s!%n", destination);
+
+            destination = scanner.nextLine();
+        }
+
+
+
+    }
+}
