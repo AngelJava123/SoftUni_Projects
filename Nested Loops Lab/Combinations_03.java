a5654777f1a4440042f7f2178efa3f76210cbcf3
AngelJava123 <88735287+AngelJava123@users.noreply.github.com>
2021-08-29 23:24:02 +0300

    Add files via upload

---
+++ Nested Loops Lab/Combinations_03.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Combinations_03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        int validCombinationsCount = 0;
+        for (int number1 = 0; number1 <= n ; number1++) {
+            for (int number2 = 0; number2 <= n ; number2++) {
+                for (int number3 = 0; number3 <= n ; number3++) {
+                    if (number1 + number2 + number3 == n) {
+                        validCombinationsCount++;
+                    }
+                }
+                }
+            }
+        System.out.println(validCombinationsCount);
+        }
+    }
