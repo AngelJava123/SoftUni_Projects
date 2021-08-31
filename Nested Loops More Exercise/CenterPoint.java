7cd6ff44fbe11607d82faf5d504ee05d37905057
AngelJava123 <88735287+AngelJava123@users.noreply.github.com>
2021-08-31 07:14:59 +0300

    Add files via upload

---
+++ Nested Loops More Exercise/CenterPoint.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class CenterPoint {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double x1 = Double.parseDouble(scanner.nextLine());
+        double y1 = Double.parseDouble(scanner.nextLine());
+
+        double x2 = Double.parseDouble(scanner.nextLine());
+        double y2 = Double.parseDouble(scanner.nextLine());
+
+        printClosestToTheCenterPoints(x1, y1, x2, y2);
+    }
+
+    private static void printClosestToTheCenterPoints(double x1, double y1, double x2, double y2) {
+
+        double result1 = Math.abs(x1) + Math.abs(y1);
+        double result2 = Math.abs(x2) + Math.abs(y2);
+
+        if (result1 < result2) {
+            System.out.printf("(%.0f, %.0f)",x1, y1);
+        } else if (result1 == result2) {
+            System.out.printf("(%.0f, %.0f)",x1, y1);
+        } else {
+            System.out.printf("(%.0f, %.0f)",x2, y2);
+        }
+    }
+}
