4b7c32c699e9f17cb0f7968916a2705e3ae03cc1
AngelJava123 <88735287+AngelJava123@users.noreply.github.com>
2021-08-31 07:09:49 +0300

    Add files via upload

---
+++ Nested Loops Exercise/NumberPyramid_01.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class NumberPyramid_01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt( scanner.nextLine());
+        int count = 0;
+
+        for ( int i = 1; i <= n; i++) {
+
+            for ( int k = 1; k <= i; k++ ) {
+                count++;
+                System.out.printf("%d ", count);
+                if (count == n ) return;
+            }
+            System.out.println();
+        }
+    }
+}
