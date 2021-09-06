4b473f25984c2ab51f50504aca5f5485ac058ac2
AngelJava123 <88735287+AngelJava123@users.noreply.github.com>
2021-09-06 22:39:46 +0300

    Add files via upload

---
+++ Data Types And Variables Exercise/BeerKegs.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class BeerKegs {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int countKeg = Integer.parseInt(scanner.nextLine());
+
+        double maxVolume = Integer.MIN_VALUE;
+
+        String maxModel = "";
+
+        for (int keg = 1; keg <= countKeg ; keg++) {
+
+            String model = scanner.nextLine();
+            double radius = Double.parseDouble(scanner.nextLine());
+            int height = Integer.parseInt(scanner.nextLine());
+
+            double volume = Math.PI * Math.pow(radius, 2) * height;
+
+            if (volume > maxVolume) {
+                maxVolume = volume;
+                maxModel = model;
+            }
+        }
+        System.out.printf("%s", maxModel);
+    }
+}
