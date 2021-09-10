b86667fb897e8473e094e2213cbaca7d4f121a96
AngelJava123 <88735287+AngelJava123@users.noreply.github.com>
2021-09-10 22:41:37 +0300

    Add files via upload

---
+++ While Loop Exercise/MaxNumber_06.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class MaxNumber_06 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int max = Integer.MIN_VALUE;
+
+        String text = scanner.nextLine();
+
+        while (!text.equals("Stop")) {
+            int number = Integer.parseInt(text);
+
+            if (number > max) {
+                max = number;
+            }
+            text = scanner.nextLine();
+        }
+        System.out.println(max);
+    }
+}
