3942d0104af48c3b5d5fb710f8543cfd027eb3a0
AngelJava123 <88735287+AngelJava123@users.noreply.github.com>
2021-09-11 00:05:12 +0300

    Add files via upload

---
+++ Lists Exercise/AppendArrays.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class AppendArrays {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<String> array = Arrays.stream(scanner.nextLine().split("\\|")).collect(Collectors.toList());
+
+        Collections.reverse(array);
+
+        System.out.println(array.toString().replace("[", "").
+                                            replace("]", "").
+                                            replace(",", "").
+                                            trim().
+                                            replaceAll(" \\s+", " "));
+    }
+}
