091e10276d553a0ab9cae840e15a42c9f5cf3960
AngelJava123 <88735287+AngelJava123@users.noreply.github.com>
2022-07-13 22:52:58 +0300

    Delete Spring Advanced/Final Spring Project directory

--- Spring Advanced/Final Spring Project/src/main/java/com/example/final_project/model/enums/Amenities.java
+++
@@ -1,5 +0,0 @@
-package com.example.final_project.model.enums;
-
-public enum Amenities {
-    SPA, FITNESS, SOLARIUM, SWIMMING_POOL
-}
--- Spring Advanced/Final Spring Project/src/main/java/com/example/final_project/model/enums/RoleEnum.java
+++
@@ -1,5 +0,0 @@
-package com.example.final_project.model.enums;
-
-public enum RoleEnum {
-    USER, ADMIN
-}
