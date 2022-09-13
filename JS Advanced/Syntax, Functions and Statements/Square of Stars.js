2ff0db8925a81a1bf6afaa3a5fb61911a8d16f2f
AngelJava123 <88735287+AngelJava123@users.noreply.github.com>
2022-09-13 20:23:03 +0300

    Update Square of Stars.js

--- JS Advanced/Syntax, Functions and Statements/Square of Stars.js
+++ JS Advanced/Syntax, Functions and Statements/Square of Stars.js
@@ -4,7 +4,7 @@ function solve(num = 5) {
 
     for (let i = 0; i < num; i++) {
         for (let j = 0; j < num; j++) {
-          pattern += " * ";
+          pattern += "* ";
         }
         pattern += '\n';
     }
