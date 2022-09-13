ace2554fc90c9d3697152bdfe1ca48c63cbb248a
AngelJava123 <88735287+AngelJava123@users.noreply.github.com>
2022-09-13 07:39:35 +0300

    Add files via upload

---
+++ JS Advanced/Syntax, Functions and Statements/Square of Stars.js
@@ -0,0 +1,23 @@
+function solve(num = 5) {
+
+    let pattern = "";
+
+    for (let i = 0; i < num; i++) {
+        for (let j = 0; j < num; j++) {
+          pattern += " * ";
+        }
+        pattern += '\n';
+    }
+
+    console.log(pattern);
+
+}
+solve(1);
+
+solve(2);
+
+solve(5);
+
+solve(7);
+
+solve();
