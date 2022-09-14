95f6839a2c56201ae389e2d79849bb1305679d41
AngelJava123 <88735287+AngelJava123@users.noreply.github.com>
2022-09-14 12:40:51 +0300

    Update Greatest Common Divisor - GCD.js

--- JS Advanced/Syntax, Functions and Statements/Greatest Common Divisor - GCD.js
+++ JS Advanced/Syntax, Functions and Statements/Greatest Common Divisor - GCD.js
@@ -12,3 +12,18 @@ function gcd_two_numbers(x, y) {
   
   console.log(gcd_two_numbers(15, 5));
   console.log(gcd_two_numbers(2154, 458));
+
+//Second Solution
+
+function gcd2(num1, num2) {
+    let gcd = num2;
+
+    while (!(num1 % gcd == 0 && num2 % gcd == 0)) {
+        gcd--;
+    }
+
+    console.log(gcd);
+  }
+
+  gcd2(15, 5);
+  gcd2(2154, 458);
