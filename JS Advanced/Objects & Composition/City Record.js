b488fb89051b5827b52173012c07e957b6c8d006
AngelJava123 <88735287+AngelJava123@users.noreply.github.com>
2022-09-19 23:17:50 +0300

    Add files via upload

---
+++ JS Advanced/Objects & Composition/City Record.js
@@ -0,0 +1,18 @@
+function solve(name, population, treasury) {
+    return {
+        name,
+        population,
+        treasury
+    }
+}
+
+
+console.log(solve('Tortuga',
+7000,
+15000
+));
+
+console.log(solve('Santo Domingo',
+12000,
+23500
+));
