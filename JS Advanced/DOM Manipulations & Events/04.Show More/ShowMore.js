b46b0ae7c4ece05c70aa02961a8f5e7471edb547
AngelJava123 <88735287+AngelJava123@users.noreply.github.com>
2022-09-25 01:19:29 +0300

    Add files via upload

---
+++ JS Advanced/DOM Manipulations & Events/04.Show More/ShowMore.js
@@ -0,0 +1,9 @@
+function showText() {
+    let more = document.getElementById('more');
+    let fullText = document.getElementById('text');
+
+    more.style.display = 'none';
+    fullText.style.display = 'inline';
+
+    debugger;
+}
