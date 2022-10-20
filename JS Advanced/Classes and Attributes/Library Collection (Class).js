aaccdaafb3ecd98dd15fcdc23728839873cbdcf8
AngelJava123 <88735287+AngelJava123@users.noreply.github.com>
2022-10-20 21:14:02 +0300

    Update Library Collection (Class).js

--- JS Advanced/Classes and Attributes/Library Collection (Class).js
+++ JS Advanced/Classes and Attributes/Library Collection (Class).js
@@ -69,9 +69,13 @@ class LibraryCollection {
 
 }
 
-const library = new LibraryCollection(2)
-console.log(library.addBook('Don Quixote', 'Miguel de Cervantes'));
-console.log(library.getStatistics('Miguel de Cervantes'));
+const library = new LibraryCollection(5)
+library.addBook('Don Quixote', 'Miguel de Cervantes');
+library.payBook('Don Quixote');
+library.addBook('In Search of Lost Time', 'Marcel Proust');
+library.addBook('Ulysses', 'James Joyce');
+console.log(library.getStatistics());
+
 
 
 
