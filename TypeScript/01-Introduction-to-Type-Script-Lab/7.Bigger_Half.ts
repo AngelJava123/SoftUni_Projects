3fcd637264fd537913cf93d9debd308ad8514b19
Angel Tashev <88735287+AngelJava123@users.noreply.github.com>
2024-04-05 07:27:23 +0300

    Add files via upload

---
+++ TypeScript/01-Introduction-to-Type-Script-Lab/7.Bigger_Half.ts
@@ -0,0 +1,14 @@
+function biggerHalf(numbers: number[]): number[] {
+
+    let sortedArray: number[] = numbers.sort((n1,n2) => n1 - n2);
+
+    const middle = Math.floor(sortedArray.length / 2);
+
+    const right = sortedArray.slice(middle);
+
+    return right;
+    
+}
+
+console.log(biggerHalf([4, 7, 2, 5]));
+console.log(biggerHalf([3, 19, 14, 7, 2, 19, 6]));
