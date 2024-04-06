09cc33a363866402601728dde56e17070e844ae0
Angel Tashev <88735287+AngelJava123@users.noreply.github.com>
2024-04-06 10:29:54 +0300

    Add files via upload

---
+++ TypeScript/01-Introduction-to-Type-Script-Lab/dist/10.Aggregate_Elements.js
@@ -0,0 +1,16 @@
+function aggregate(arr) {
+    let sumation = 0;
+    let inverseSum = 0;
+    let concatenation = '';
+    for (let index = 0; index < arr.length; index++) {
+        const element = arr[index];
+        sumation += element;
+        inverseSum += 1 / element;
+        concatenation += element.toString();
+    }
+    console.log(sumation);
+    console.log(inverseSum);
+    console.log(concatenation);
+}
+aggregate([1, 2, 3]);
+aggregate([2, 4, 8, 16]);
