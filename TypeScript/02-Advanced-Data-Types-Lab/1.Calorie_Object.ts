5d2f663a408962e272b8dec8131bd6ad41f54bde
Angel Tashev <88735287+AngelJava123@users.noreply.github.com>
2024-04-09 07:55:32 +0300

    Add files via upload

---
+++ TypeScript/02-Advanced-Data-Types-Lab/1.Calorie_Object.ts
@@ -0,0 +1,15 @@
+function inputProduct(arr: string[]): void {
+
+    const products: { [productName: string]: string} = {};
+
+    for (let index = 0; index < arr.length; index+=2) {
+        const element = arr[index];
+        const calories = arr[index + 1];
+        
+        products[element] = calories;
+    }
+    console.log(products);
+}
+
+inputProduct(['Yoghurt', '48', 'Rise', '138','Apple', '52']);
+inputProduct(['Potato', '93', 'Skyr', '63','Cucumber', '18', 'Milk', '42'])
