7b9462d3bb20333ee842ec9489c645bfa84a0e5b
Angel Tashev <88735287+AngelJava123@users.noreply.github.com>
2024-04-10 00:33:31 +0300

    Add files via upload

---
+++ TypeScript/02-Advanced-Data-Types-Lab/dist/3.Inventory.js
@@ -0,0 +1,34 @@
+function inputInventory(array) {
+    const people = [];
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        const splitElement = element.split('/');
+        const newPerson = {
+            Hero: splitElement[0],
+            level: parseInt(splitElement[1]),
+            items: splitElement[2] ? splitElement[2] : undefined
+        };
+        people.push(newPerson);
+    }
+    // Sort people array by level in ascending order
+    people.sort((a, b) => a.level - b.level);
+    // Print each person in the desired format
+    for (let index = 0; index < people.length; index++) {
+        const person = people[index];
+        console.log(`Hero: ${person.Hero}`);
+        console.log(`level => ${person.level}`);
+        if (person.items) {
+            console.log(`items => ${person.items}`);
+        }
+    }
+}
+inputInventory([
+    'Isacc / 25 / Apple, GravityGun',
+    'Derek / 12 / BarrelVest, DestructionSword',
+    'Hes / 1 / Desolator, Sentinel, Antara'
+]);
+inputInventory([
+    'Batman / 2 / Banana, Gun',
+    'Superman / 18 / Sword',
+    'Poppy / 28 / Sentinel, Antara'
+]);
