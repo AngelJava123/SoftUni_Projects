112c2d50663a4530a1079b6bd3ec5c13864ad19e
Angel Tashev <88735287+AngelJava123@users.noreply.github.com>
2024-04-11 10:06:36 +0300

    Add files via upload

---
+++ TypeScript/02-Advanced-Data-Types-Lab/dist/5.Town_Population.js
@@ -0,0 +1,50 @@
+function inputTownPopulation(array) {
+    ;
+    const result = [];
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        const splitElement = element.split(' <-> ');
+        const townName = splitElement[0];
+        const townPopulation = parseInt(splitElement[1]);
+        let found = false;
+        for (let i = 0; i < result.length; i++) {
+            const obj = result[i];
+            if (obj.townName === townName) {
+                obj.townPopulation += townPopulation;
+                found = true;
+                break;
+            }
+            ;
+        }
+        ;
+        if (!found) {
+            const townPpl = {
+                townName: townName,
+                townPopulation: townPopulation
+            };
+            result.push(townPpl);
+        }
+        ;
+    }
+    ;
+    for (let index = 0; index < result.length; index++) {
+        const town = result[index];
+        console.log(`${town.townName} : ${town.townPopulation}`);
+    }
+    ;
+}
+;
+inputTownPopulation([
+    'Sofia <-> 1200000',
+    'Montana <-> 20000',
+    'New York <-> 10000000',
+    'Washington <-> 2345000',
+    'Las Vegas <-> 1000000'
+]);
+inputTownPopulation([
+    'Istanbul <-> 100000',
+    'Honk Kong <-> 2100004',
+    'Jerusalem <-> 2352344',
+    'Mexico City <-> 23401925',
+    'Istanbul <-> 1000',
+]);
