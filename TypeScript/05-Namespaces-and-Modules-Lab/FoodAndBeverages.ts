6d35d150e7c4e896d29c56f249a12e7d1a5a81bd
Angel Tashev <88735287+AngelJava123@users.noreply.github.com>
2024-04-30 22:59:57 +0300

    Add files via upload

---
+++ TypeScript/05-Namespaces-and-Modules-Lab/FoodAndBeverages.ts
@@ -0,0 +1,10 @@
+namespace FoodAndBeverages {
+
+    export interface Delivery {
+
+        newCustomer(customerName: string, visited: boolean): string;
+        visitCustomer(customerName: string): void;
+        showCustomers(): string;
+
+    }
+}
